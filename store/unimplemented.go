af3c8189dc1502a336d222f5aa4d78b8df0b6dac
ZalgoNoise <zalgo.noise@gmail.com>
2022-12-07 02:40:22 +0100

    fix: store repo update

--- store/unimplemented.go
+++ store/unimplemented.go
@@ -21,8 +21,8 @@ func (u unimplemented) List(ctx context.Context) ([]*Record, error) {
 	return nil, ErrUnimplemented
 }
 
-// FilterByTypeAndDomain implements the store.Repository interface
-func (u unimplemented) FilterByTypeAndDomain(ctx context.Context, rtype, domain string) (*Record, error) {
+// FindByTypeAndDomain implements the store.Repository interface
+func (u unimplemented) FindByTypeAndDomain(ctx context.Context, rtype, domain string) (*Record, error) {
 	return nil, ErrUnimplemented
 }
 
@@ -41,8 +41,18 @@ func (u unimplemented) Update(ctx context.Context, domain string, r *Record) err
 	return ErrUnimplemented
 }
 
-// Delete implements the store.Repository interface
-func (u unimplemented) Delete(ctx context.Context, r *Record) error {
+// DeleteByAddress removes all records with IP address `addr`
+func (u unimplemented) DeleteByAddress(ctx context.Context, addr string) error {
+	return ErrUnimplemented
+}
+
+// DeleteByDomain removes all records with domain name `name`
+func (u unimplemented) DeleteByDomain(ctx context.Context, name string) error {
+	return ErrUnimplemented
+}
+
+// DeleteByTypeAndDomain removes all records with record type `rtype` and domain name `name`
+func (u unimplemented) DeleteByTypeAndDomain(ctx context.Context, rtype, name string) error {
 	return ErrUnimplemented
 }
 
