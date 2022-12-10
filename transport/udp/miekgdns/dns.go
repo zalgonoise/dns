43c2124285e7d312cadf95494bcdef4745673d59
zalgonoise <zalgo.noise@gmail.com>
2022-12-10 23:37:46 +0100

    fix: remove WithLogger from HTTP / UDP

--- transport/udp/miekgdns/dns.go
+++ transport/udp/miekgdns/dns.go
@@ -4,17 +4,15 @@ import (
 	"github.com/miekg/dns"
 	"github.com/zalgonoise/dns/service"
 	"github.com/zalgonoise/dns/transport/udp"
-	"github.com/zalgonoise/logx"
 )
 
 // udps implements the udp.Server interface
 type udps struct {
-	on     bool
-	ans    service.Answering
-	conf   *udp.DNS
-	srv    *dns.Server
-	err    error
-	logger logx.Logger
+	on   bool
+	ans  service.Answering
+	conf *udp.DNS
+	srv  *dns.Server
+	err  error
 }
 
 // NewServer returns a github.com/miekg/dns implementation of udp.Server
@@ -29,23 +27,5 @@ func NewServer(conf *udp.DNS, s service.Answering) udp.Server {
 	return &udps{
 		conf: conf,
 		ans:  s,
-	}
-}
-
-// WithTrace returns the same UDP server configured with a logger
-func WithLogger(server udp.Server, logger logx.Logger) udp.Server {
-	if server == nil {
-		return nil
-	}
-	if _, ok := server.(*udps); !ok {
-		return server
-	}
-	if logger == nil {
-		logger = logx.Default()
-	}
-	return &udps{
-		conf:   server.(*udps).conf,
-		ans:    server.(*udps).ans,
-		logger: logger,
 	}
 }
